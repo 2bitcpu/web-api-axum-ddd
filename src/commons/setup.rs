0a0a8c3345395a9d557711aa5507b93730f0671e
2bitcpu <84998510+2bitcpu@users.noreply.github.com>
2025-04-04 23:34:14 +0900

    tracing

--- src/commons/setup.rs
+++ src/commons/setup.rs
@@ -1,5 +1,6 @@
 use crate::commons::types::{BoxError, DbPool};
 use sqlx::migrate::MigrateDatabase;
+use tracing_subscriber::{EnvFilter, fmt::time::ChronoLocal};
 
 pub async fn initialize_db(db_url: &str) -> Result<DbPool, BoxError> {
     if db_url != "sqlite::memory:" {
@@ -52,4 +53,17 @@ pub async fn initialize_db(db_url: &str) -> Result<DbPool, BoxError> {
     .await?;
 
     Ok(pool)
+}
+
+pub fn init_tracing() {
+    tracing_subscriber::fmt()
+        .with_timer(ChronoLocal::rfc_3339())
+        .with_env_filter(
+            EnvFilter::builder()
+                .try_from_env()
+                .unwrap_or_else(|_| EnvFilter::new("error")),
+        )
+        .with_file(true)
+        .with_line_number(true)
+        .init();
 }
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,21 @@ use tokio::net::TcpListener;
 use web_api::commons::setup::initialize_db;
 use web_api::commons::{
     config::{DB_URL, HOST_NAME},
+    setup,
     types::{BoxError, DbPool},
 };
 use web_api::handlers::create_handlers;
 
 #[tokio::main]
 async fn main() -> Result<(), BoxError> {
+    setup::init_tracing();
+
     let pool: DbPool = initialize_db(&*DB_URL).await?;
 
     let app = create_handlers(pool);
 
     let listener = TcpListener::bind(&*HOST_NAME).await?;
-    println!("Listening on http://{}", listener.local_addr().unwrap());
+    tracing::info!("Listening on {}", listener.local_addr()?);
 
     axum::serve(listener, app.into_make_service()).await?;
 
